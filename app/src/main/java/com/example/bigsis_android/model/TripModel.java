fc9dd330ecf3e0ac34b5642d567be62a84f2eecf
liliajerbi <lilia-jerbi@hotmail.fr>
2019-09-04 08:52:19 +0200

    classpath of gms service changed

---
+++ app/src/main/java/com/example/bigsis_android/model/TripModel.java
@@ -0,0 +1,49 @@
+package com.example.bigsis_android.model;
+
+public class TripModel {
+
+    private String fromLocation;
+    private String toLocation;
+    // TODO dateTime or timestamp, author and chat
+    private String thumbnail;
+    private int maxParticipants;
+
+    public TripModel(String fromLocation, String toLocation, String thumbnail, int maxParticipants) {
+        this.fromLocation = fromLocation;
+        this.toLocation = toLocation;
+        this.thumbnail = thumbnail;
+        this.maxParticipants = maxParticipants;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+}
