e23bcc2fab348dc55ad01ad72d79f74cd161fef6
liliajerbi <lilia-jerbi@hotmail.fr>
2019-09-18 17:06:22 +0200

    design reviewed

--- app/src/main/java/fr/bigsis/android/activity/TripListActivity.java
+++ app/src/main/java/fr/bigsis/android/activity/TripListActivity.java
@@ -10,7 +10,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
