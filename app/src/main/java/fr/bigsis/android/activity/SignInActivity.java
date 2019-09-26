c3044475408d76b17b81ab91388ebb937241a381
liliajerbi <lilia-jerbi@hotmail.fr>
2019-09-26 16:33:56 +0200

    unusable files deleted

--- app/src/main/java/fr/bigsis/android/activity/SignInActivity.java
+++ app/src/main/java/fr/bigsis/android/activity/SignInActivity.java
@@ -4,9 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -14,26 +12,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.Objects;
-
-import fr.bigsis.android.Constants;
 import fr.bigsis.android.R;
-import fr.bigsis.android.repository.UserRepository;
 
 public class SignInActivity extends AppCompatActivity {
 
     EditText emailBox, passwordBox;
     Button btSignIn;
-    UserRepository authentication;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
