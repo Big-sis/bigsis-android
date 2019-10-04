34f08e622a1f1c163c8dae8c10ee79a03b32a11e
liliajerbi <lilia-jerbi@hotmail.fr>
2019-10-04 10:51:23 +0200

    setting the alert dialog for the reset password

--- app/src/main/java/fr/bigsis/android/activity/SignInActivity.java
+++ app/src/main/java/fr/bigsis/android/activity/SignInActivity.java
@@ -1,16 +1,19 @@
 package fr.bigsis.android.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,9 +26,9 @@ public class SignInActivity extends AppCompatActivity {
 
     EditText emailBox, passwordBox;
     Button btSignIn;
+    TextView tvForgotPassword;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    TextView tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,17 +75,18 @@ public class SignInActivity extends AppCompatActivity {
         String password = passwordBox.getText().toString();
 
         if (TextUtils.isEmpty(email)) {
-            emailBox.setError("Veuillez entrer votre adresse e-mail");
+            emailBox.setError(getString(R.string.email_adress));
             emailBox.requestFocus();
-        } if ( TextUtils.isEmpty(password)){
-            passwordBox.setError("Veuillez entrer votre mot de passe");
+        }
+        if (TextUtils.isEmpty(password)) {
+            passwordBox.setError(getString(R.string.enter_password));
             passwordBox.requestFocus();
         } else {
             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (!task.isSuccessful()) {
-                        Toast.makeText(SignInActivity.this, "mot de passe ou e-mail incorrect", Toast.LENGTH_LONG).show();
+                        Toast.makeText(SignInActivity.this, R.string.email_or_password_incorrect, Toast.LENGTH_LONG).show();
                     }
                 }
             });
@@ -90,18 +94,34 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void resetPassword() {
-        String email = emailBox.getText().toString().trim();
-        if (TextUtils.isEmpty(email)) {
-            emailBox.setError("Veuillez indiquer votre adresse e-mail");
-            emailBox.requestFocus();
-            return;
-        }
-        mAuth.setLanguageCode("fr");
-        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null);
+        final AppCompatEditText input = view.findViewById(R.id.editText);
+        AlertDialog builder = new AlertDialog.Builder(this, R.style.AlertDialogStyle)
+                .setTitle(R.string.forgot_password)
+                .setMessage(getString(R.string.email_adress))
+                .setPositiveButton(getString(R.string.send), null)
+                .setNegativeButton(getString(R.string.cancel), null)
+                .setView(view)
+                .show();
+
+        Button positiveButton = builder.getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(SignInActivity.this, "e-mail envoyé", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                String email = input.getText().toString();
+                if (email.isEmpty()) {
+                    input.setError(getString(R.string.email_adress));
+                    input.requestFocus();
+                } else {
+                    mAuth.setLanguageCode("fr");
+                    mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignInActivity.this, R.string.email_was_sent, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
                 }
             }
         });
